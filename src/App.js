0b15a40c86452bfac3ab10c217271c4899ffcc6e
Bikash Shaw <1985.bikash.shaw@gmail.com>
2023-03-02 11:27:30 +0530

    Search functionality Implemented

--- src/App.js
+++ src/App.js
@@ -6,27 +6,50 @@ class App extends Component {
     super();
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
       response.json().then((user) =>
-        this.setState(() => {
-          return {
-            monsters: user,
-          };
-        })
+        this.setState(
+          () => {
+            return {
+              monsters: user,
+            };
+          },
+          () => {
+            console.log(this.state.monsters);
+          }
+        )
       )
     );
   }
   render() {
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => {
+        <h1>Monster Rolodex</h1>
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search Monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchField = event.target.value.toLowerCase();
+
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
-              <h1>{monster.name}</h1>
+              <h3>{monster.name}</h3>
             </div>
           );
         })}
